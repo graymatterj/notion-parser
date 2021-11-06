4ef60f23a5c61c59cbbbb6591bba8d3b2470a9ce
graymatterj <github@thegrays.co.uk>
2021-11-06 23:00:56 +0000

    Load .env values into application

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"log"
+	"os"
+	"github.com/joho/godotenv"
+)
+
+var EnvNotFound = "Could not load .env"
+
+func loadDotEnv(fileName string) {
+	err := godotenv.Load(fileName)
+
+	if err != nil {
+		log.Fatal(EnvNotFound)
+	}
+}
+
+func main() {
+	loadDotEnv(".env")
+
+	path := os.Getenv("NOTION_API_PATH")
+	key := os.Getenv("NOTION_API_KEY")
+	version := os.Getenv("NOTION_API_VERSION")
+	page := os.Getenv("NOTION_PAGE_ID")
+
+}
